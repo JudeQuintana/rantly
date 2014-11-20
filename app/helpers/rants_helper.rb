feea2bf46e4e5b7d67d12645e854035c181e04fa
Jude Quintana <jude.quintana@gmail.com>
2014-11-20 13:28:10 -0700

    implemented markdown in rants

--- app/helpers/rants_helper.rb
+++ app/helpers/rants_helper.rb
@@ -4,4 +4,9 @@ module RantsHelper
     rant_collection.sort_by { |rant| rant.favorites.count }.reverse
   end
 
+  def markdown(content)
+    @markdown ||= Redcarpet::Markdown.new(Redcarpet::Render::HTML, autolink: true, space_after_headers: true, fenced_code_blocks: true, strikethrough: true, underline: true, quote: true)
+    @markdown.render(content)
+  end
+
 end
